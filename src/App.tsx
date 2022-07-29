02cdfc2e69d52d4821fec8a8e82676d0d9201b3c
johnny.hughes <johnny.hughes@autotrader.co.uk>
2022-07-29 11:27:03 +0100

    Eslint setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css';
-import {Main} from "./main";
+import React from 'react'
+
+import './App.css'
+import { Main } from './main'
 
 function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Main />
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+import { FirstComponent } from './components/FirstComponent'
+
 export const Main = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/first" element={<FirstComponent />} />
+            </Routes>
+        </Router>
     )
 }
